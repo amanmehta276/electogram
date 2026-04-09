9ad9efba59e156942b0f55028e7d8e29231efac2
amanmehta276 <166599351+amanmehta276@users.noreply.github.com>
2026-04-10 02:08:14 +0530

    fix

---
+++ my-circuit/index.circuit.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default () => (
+  <board width="40mm" height="30mm">
+
+    <resistor 
+      name="R1" 
+      resistance="220ohm" 
+      footprint="0805"
+      pcbX={0}
+      pcbY={10}
+    />
+
+    <led 
+      name="LED1" 
+      color="red" 
+      footprint="0603"
+      pcbX={0}
+      pcbY={-10}
+    />
+
+    <trace from="net.VCC" to=".R1 > .pin1" />
+    <trace from=".R1 > .pin2" to=".LED1 > .pin1" />
+    <trace from=".LED1 > .pin2" to="net.GND" />
+  </board>
+)
